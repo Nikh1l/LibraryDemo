1c7e6cdfcb6bd526b86ba11a8e83d957e63df752
Nikhil <nk20997@gmail.com>
2020-12-23 16:48:43 +0530

    adding common library

--- app/src/main/java/com/nevergiveup/demoapp/MainActivity.kt
+++ app/src/main/java/com/nevergiveup/demoapp/MainActivity.kt
@@ -13,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         toastMessage.s(
             this,
             "This is a toast message",
